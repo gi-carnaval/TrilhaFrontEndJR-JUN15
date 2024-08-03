08b61b4f3d187f44fd71db4cfd5ab012c88ca385
Giovani Carnaval <gi.carnaval91@gmail.com>
2024-08-03 04:52:15 -0300

    refactor(styles): refactoring styles

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import "./Hero.scss"
 
 import giovaniProfileTwo from '../../assets/giovani-2.png'
 import { Parallax } from "react-scroll-parallax"
-import { Fade, Slide } from "react-awesome-reveal"
+import { Slide } from "react-awesome-reveal"
 import HeroTitle from "../HeroTitle"
 import Paragraph from "../Paragraph"
 
@@ -23,36 +23,31 @@ export default function Hero() {
 
         <HeroTitle />
         <Parallax speed={10} >
-          <Fade duration={1000} triggerOnce>
-            <Slide direction="up" triggerOnce>
-              <Paragraph>
-                {`
+          <Slide direction="up" triggerOnce>
+            <Paragraph>
+              {`
                   Sou um desenvolvedor Frontend de ${calcAge()} anos com mais de ${getExperienceYears()} anos de experiência em desenvolvimento de websites e sistemas web.
                   Focado em ReactJS e NextJS, criando interfaces de usuário responsivas e intuitivas.
                   Tenho um forte foco em desempenho e otimização contínua das aplicações.
                 `
-                }
-
-              </Paragraph>
-              <Paragraph>
-                Estou sempre buscando novos desafios que me permitam aprender e exercitar minhas habilidades e conhecimentos em tecnologia.
-                Minhas áreas de interesse na programação incluem a criação de soluções escaláveis e de alta qualidade, além da colaboração com equipes multidisciplinares.
-                Meu objetivo de carreira é crescer continuamente como desenvolvedor, adquirindo novas competências e contribuindo para projetos inovadores.
-              </Paragraph>
-            </Slide>
-          </Fade>
+              }
+            </Paragraph>
+            <Paragraph>
+              Estou sempre buscando novos desafios que me permitam aprender e exercitar minhas habilidades e conhecimentos em tecnologia.
+              Minhas áreas de interesse na programação incluem a criação de soluções escaláveis e de alta qualidade, além da colaboração com equipes multidisciplinares.
+              Meu objetivo de carreira é crescer continuamente como desenvolvedor, adquirindo novas competências e contribuindo para projetos inovadores.
+            </Paragraph>
+          </Slide>
         </Parallax>
       </div>
       <div className="image-hero">
-        <Fade duration={1000} delay={250}>
-          <Slide direction="right" >
-            <Parallax speed={-10}>
-              <a href="https://www.instagram.com/giovani_carnaval/" target="blank" title="Acessar meu instagram">
-                <img className="profile-1 profileImg" src={giovaniProfileTwo} alt="" />
-              </a>
-            </Parallax>
-          </Slide>
-        </Fade>
+        <Slide direction="right" >
+          <Parallax speed={-10}>
+            <a href="https://www.instagram.com/giovani_carnaval/" target="blank" title="Acessar meu instagram">
+              <img className="profile-1 profileImg" src={giovaniProfileTwo} alt="" />
+            </a>
+          </Parallax>
+        </Slide>
       </div>
     </div >
   )
