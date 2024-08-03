bbf9476b40271faaaca182877a2c4c9b1ac1fd22
Giovani Carnaval <gi.carnaval91@gmail.com>
2024-08-03 02:30:42 -0300

    refactor(motivations): refactoring motivations section

--- src/components/Motivations/Motivations.tsx
+++ src/components/Motivations/Motivations.tsx
@@ -2,21 +2,32 @@ import { motivationStatements } from "../../data/motivations";
 import Paragraph from "../Paragraph";
 import SectionTitle from "../SectionTitle";
 import "./Motivations.scss"
+import imgProfile2 from '../../assets/giovani-1.png'
+import { Fade, Slide } from "react-awesome-reveal";
 
 export default function Motivations() {
   return (
-    <div className="motivationSection">
+    <div className="motivationSection" id="motivacoes">
       <div className="motivationCard">
-        <SectionTitle>Motivações</SectionTitle>
-        <div>
-          {
-            motivationStatements.map(motivation => (
-              <Paragraph >
-                {motivation}
-              </Paragraph>
-            ))
-          }
+        <div className="motivationCardTexts">
+          <SectionTitle>Motivações</SectionTitle>
+          <div className="motivationCardTextsParagraphs">
+            {
+              motivationStatements.map((motivation, index) => (
+                <Fade key={index}>
+                  <Slide direction="up" fraction={0}>
+                    <Paragraph >
+                      {motivation}
+                    </Paragraph>
+                  </Slide>
+                </Fade>
+              ))
+            }
+          </div>
         </div>
+        <Fade>
+          <img className="imgProfile2" src={imgProfile2} alt="Giovani Carnaval" />
+        </Fade>
       </div>
     </div>
   )
